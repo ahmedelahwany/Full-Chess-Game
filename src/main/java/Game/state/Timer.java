829b1ca93c3a68e95d19772112ef68df56d9e9bd
ahmedelahwany <ahmed.elahwany26@gmail.com>
2021-07-09 20:50:53 +0200

    add Timer.java for to model for implementing a chess clock

---
+++ src/main/java/Game/state/Timer.java
@@ -0,0 +1,41 @@
+package Game.state;
+
+public class Timer {
+
+    private String minutes;
+    private String seconds;
+
+    public Timer( ) {
+        this.minutes = "";
+        this.seconds = "seconds";
+    }
+
+    public String getMinutes() {
+        return this.minutes;
+    }
+
+    public String getSeconds() {
+        return this.seconds;
+    }
+
+    public void setMinutes(int min) {
+        if (min < 10) {
+            this.minutes = "0" + min;
+        } else {
+            this.minutes = min + "";
+        }
+    }
+
+    public void setSeconds(int sec) {
+        if (sec < 10) {
+            this.seconds = "0" + sec;
+        } else {
+            this.seconds = sec + "";
+        }
+    }
+
+    public String toString() {
+        return this.minutes + ":" + this.seconds;
+    }
+
+}
